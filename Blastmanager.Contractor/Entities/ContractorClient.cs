e40d346daf5b7ff8e40ec0a41b703097194fbbd7
Kristoffer Tunlid <Kristoffer.Tunlid@infobric.com>
2023-11-17 13:51:46 +0100

    Added ContractorClient.cs for API calls

--- Blastmanager.Contractor/Entities/ContractorClient.cs
+++ Blastmanager.Contractor/Entities/ContractorClient.cs
@@ -1,65 +1,88 @@
-﻿using Blastmanager.Contractor.Entities.Interfaces;
+﻿using System.Net.Http.Json;
+using Blastmanager.Contractor.Entities.Constants;
+using Blastmanager.Contractor.Entities.Interfaces;
+using Blastmanager.Contractor.Entities.Response;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Blastmanager.Contractor.Entities;
 
-public abstract class ContractorClient(IOptions<ContractorOption> options, 
+/// <inheritdoc />
+public abstract class ContractorClient(IOptions<ContractorOption> options,
                 IHttpClientFactory factory,
-                ILogger<ContractorClient> logger) 
+                ILogger<ContractorClient> logger)
         : IContractorClient
 {
     private readonly HttpClient _client = factory.CreateClient();
     private readonly ILogger<ContractorClient> _logger = logger;
     private readonly IOptions<ContractorOption> _options = options;
 
-    public async ValueTask GetProjectsAsync(bool onlyActive, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<GetProjectsResponse[]> GetProjectsAsync(bool onlyActive,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var response = await _client.GetAsync($"{ApiUri.GetProjects}?onlyActive={onlyActive}", cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var value =
+                await response.Content.ReadFromJsonAsync<GetProjectsResponse[]>(cancellationToken);
+        return value!;
     }
 
-    public void GetProjects(bool onlyActive)
+    /// <inheritdoc />
+    public async Task<BlastReportsResponse[]> GetBlastReportsByBlastDateAsync(DateTime from, DateTime to,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (from > to) throw new ArgumentException("From date must be before to date");
+        var response = await _client.GetAsync($"{ApiUri.GetBlastReportsByBlastDate}" +
+                                              $"?from={from:YYYY-MM-DDTHH:mm:ss}&to={to:YYYY-MM-DDTHH:mm:ss}",
+                cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var value =
+                await response.Content.ReadFromJsonAsync<BlastReportsResponse[]>(cancellationToken);
+        return value!;
     }
 
-    public async ValueTask GetBlastReportsByBlastDateAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<BlastReportsResponse[]> GetBlastReportsByLastChangedAsync(DateTime from, DateTime to,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (from > to) throw new ArgumentException("From date must be before to date");
+        var response = await _client.GetAsync($"{ApiUri.GetBlastReportsByLastChanged}" +
+                                              $"?from={from:YYYY-MM-DDTHH:mm:ss}&to={to:YYYY-MM-DDTHH:mm:ss}",
+                cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var value =
+                await response.Content.ReadFromJsonAsync<BlastReportsResponse[]>(cancellationToken);
+        return value!;
     }
 
-    public void GetBlastReportsByBlastDate(DateTime from, DateTime to)
+    /// <inheritdoc />
+    public async Task<GetMisfireResponse[]> GetMisfiresByLastChangedAsync(DateTime from, DateTime to,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (from > to) throw new ArgumentException("From date must be before to date");
+        var response = await _client.GetAsync($"{ApiUri.GetMisFiresByLastChanged}" +
+                                              $"?from={from:YYYY-MM-DDTHH:mm:ss}&to={to:YYYY-MM-DDTHH:mm:ss}",
+                cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var value =
+                await response.Content.ReadFromJsonAsync<GetMisfireResponse[]>(
+                        cancellationToken);
+        return value!;
     }
 
-    public async ValueTask GetBlastReportsByLastChangedAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<GetMisfireResponse[]> GetMisfiresByFoundDateAsync(DateTime from, DateTime to,
+            CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
-    }
-
-    public void GetBlastReportsByLastChanged(DateTime from, DateTime to)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async ValueTask GetMisfiresByLastChangedAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void GetMisfiresByLastChanged(DateTime from, DateTime to)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async ValueTask GetMisfiresByFoundDateAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void GetMisfiresByFoundDate(DateTime from, DateTime to)
-    {
-        throw new NotImplementedException();
+        if (from > to) throw new ArgumentException("From date must be before to date");
+        var response = await _client.GetAsync($"{ApiUri.GetMisFiresByFoundDate}" +
+                                              $"?from={from:YYYY-MM-DDTHH:mm:ss}&to={to:YYYY-MM-DDTHH:mm:ss}",
+                cancellationToken);
+        response.EnsureSuccessStatusCode();
+        var value =
+                await response.Content.ReadFromJsonAsync<GetMisfireResponse[]>(
+                        cancellationToken);
+        return value!;
     }
 }
