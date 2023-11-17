159ab74a7f46c6e9a614bf20f25a77d9f55e5bb0
Kristoffer Tunlid <Kristoffer.Tunlid@infobric.com>
2023-11-17 13:12:57 +0100

    Added ContractorOption.cs

--- Blastmanager.Contractor/Entities/ContractorClient.cs
+++ Blastmanager.Contractor/Entities/ContractorClient.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Options;
 
 namespace Blastmanager.Contractor.Entities;
 
-public abstract class ContractorClient(IOptions<object> options, 
+public abstract class ContractorClient(IOptions<ContractorOption> options, 
                 IHttpClientFactory factory,
                 ILogger<ContractorClient> logger) 
         : IContractorClient
 {
     private readonly HttpClient _client = factory.CreateClient();
     private readonly ILogger<ContractorClient> _logger = logger;
-    private readonly IOptions<object> _options = options;
+    private readonly IOptions<ContractorOption> _options = options;
 
     public async ValueTask GetProjectsAsync(bool onlyActive, CancellationToken cancellationToken = default)
     {
@@ -43,22 +43,22 @@ public abstract class ContractorClient(IOptions<object> options,
         throw new NotImplementedException();
     }
 
-    public async ValueTask GetMisFiresByLastChangedAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
+    public async ValueTask GetMisfiresByLastChangedAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public void GetMisFiresByLastChanged(DateTime from, DateTime to)
+    public void GetMisfiresByLastChanged(DateTime from, DateTime to)
     {
         throw new NotImplementedException();
     }
 
-    public async ValueTask GetMisFiresByFoundDateAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
+    public async ValueTask GetMisfiresByFoundDateAsync(DateTime from, DateTime to, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public void GetMisFiresByFoundDate(DateTime from, DateTime to)
+    public void GetMisfiresByFoundDate(DateTime from, DateTime to)
     {
         throw new NotImplementedException();
     }
---
+++ Blastmanager.Contractor/Entities/ContractorOption.cs
@@ -0,0 +1,11 @@
+﻿using Blastmanager.Entities;
+
+namespace Blastmanager.Contractor.Entities;
+
+public record ContractorOption : BlastManagerConfig
+{
+    internal ContractorOption() : base(string.Empty, string.Empty) { }
+    public ContractorOption(string username, string password, string proxy = null!) : base(username, password, proxy)
+    {
+    }
+}
