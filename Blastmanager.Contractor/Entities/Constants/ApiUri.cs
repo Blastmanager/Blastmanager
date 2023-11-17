2b9708610921599985fd64767690bfb59b6910f8
Kristoffer Tunlid <Kristoffer.Tunlid@infobric.com>
2023-11-17 13:12:18 +0100

    Added API path constants

---
+++ Blastmanager.Contractor/Entities/Constants/ApiUri.cs
@@ -0,0 +1,12 @@
+﻿namespace Blastmanager.Contractor.Entities.Constants;
+
+internal class ApiUri
+{
+    public const string BaseUri = "";
+
+    public const string GetProjects = "api/contractor/GetProjects";
+    public const string GetBlastReportsByBlastDate = "api/contractor/GetBlastReportsByBlastDate";
+    public const string GetBlastReportsByLastChanged = "api/contractor/GetBlastReportsByLastChanged";
+    public const string GetMisFiresByLastChanged = "api/contractor/GetMisfiresByLastChanged";
+    public const string GetMisFiresByFoundDate = "api/contractor/GetMisfiresByFoundDate";
+}
